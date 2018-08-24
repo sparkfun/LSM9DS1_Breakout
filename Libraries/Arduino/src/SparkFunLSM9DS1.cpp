6e7ce2743af3a0a3edbc0f170969a956e773f709
Grahldg <40771146+Grahldg@users.noreply.github.com>
2018-08-24 10:55:39 -0600

    Code optimization
    Removed unnecessary while loops.

--- Libraries/Arduino/src/SparkFunLSM9DS1.cpp
+++ Libraries/Arduino/src/SparkFunLSM9DS1.cpp
@@ -1112,8 +1112,6 @@ uint8_t LSM9DS1::I2CreadByte(uint8_t address, uint8_t subAddress)
 	Wire.write(subAddress);	                 // Put slave register address in Tx buffer
 	Wire.endTransmission(true);             // Send the Tx buffer, but send a restart to keep connection alive
 	Wire.requestFrom(address, (uint8_t) 1);  // Read one byte from slave register address 
-	while ((Wire.available() < 1) && (timeout-- > 0))
-		delay(1);
 	
 	if (timeout <= 0)
 		return 255;	//! Bad! 255 will be misinterpreted as a good value.
@@ -1132,8 +1130,7 @@ uint8_t LSM9DS1::I2CreadBytes(uint8_t address, uint8_t subAddress, uint8_t * des
 	Wire.endTransmission(true);             // Send the Tx buffer, but send a restart to keep connection alive
 	uint8_t i = 0;
 	Wire.requestFrom(address, count);  // Read bytes from slave register address 
-	while ((Wire.available() < count) && (timeout-- > 0))
-		delay(1);
+
 	if (timeout <= 0)
 		return -1;
 	
